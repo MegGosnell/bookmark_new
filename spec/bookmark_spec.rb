6a663253f1600e90dd157ed00dab17d8f9af9a95
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-28 16:29:34 +0000

    Interacting with PostgreSQL from Ruby

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,8 +7,6 @@ describe Bookmark do
             bookmarks = Bookmark.all
 
     expect(bookmarks).to include("http://www.makersacademy.com")
-    expect(bookmarks).to include("http://www.destroyallsoftware.com")
-    expect(bookmarks).to include("http://www.google.com")
         end
     end
 end
