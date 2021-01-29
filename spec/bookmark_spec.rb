ade89b736434f42c88e827a09d0a839737daad40
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-29 09:54:51 +0000

    setting up test enviroment complete

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,10 +4,11 @@ describe Bookmark do
     
     describe './all' do 
         it 'returns all bookmarks' do 
+            
         connection = PG.connect(dbname: 'bookmark_manager_test')
 
         connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-            bookmarks = Bookmarks.all
+            bookmarks = Bookmark.all
         expect(bookmarks).to include("http://www.makersacademy.com")
         end
     end
--- spec/set_up_test_database.rb
+++ spec/set_up_test_database.rb
@@ -2,7 +2,9 @@ require 'pg'
 
 
 p 'Setting up database...'
+def set_up_test_database
 
 connection = PG.connect(dbname: 'bookmark_manager_test')
 
 connection.exec("TRUNCATE bookmarks;")
+end
