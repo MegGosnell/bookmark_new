315c23f3252dd44d4086075837476efddb405f02
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-29 10:32:59 +0000

    refractoring

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,8 +1,8 @@
 feature 'Adding bookmark' do 
     scenario 'The user can add a bookmark to Bookmark manager' do 
         visit('/bookmarks/new')
-        fill_in('url', with: 'http//testbookmark.com')
+        fill_in('url', with: 'http//www.testbookmark.com')
         click_button 'Submit'
-        expect(page).to have_content 'http//testbookmark.com'
+        expect(page).to have_content 'http//www.testbookmark.com'
     end 
 end 
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -9,4 +9,15 @@ feature 'Viewing bookmarks' do
     expect(page).to have_content "http://www.makersacademy.com"
   
   end
+
+  scenario 'Visiting /bookmarks shows me all the bookmarks' do 
+    
+
+  
+  Bookmark.create(url: "http://www.makersacademy.com")
+  Bookmark.create(url: "http://www.destroyallsoftware.com")
+  Bookmark.create(url: "http://www.google.com")
+
+  visit('/bookmarks')
+  end
 end
