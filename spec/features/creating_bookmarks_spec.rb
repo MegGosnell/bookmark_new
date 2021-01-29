c46241dda7275f19fccea515fb522a400df1d1da
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-29 11:21:55 +0000

    Wrapping database data in program objects

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,7 +2,8 @@ feature 'Adding bookmark' do
     scenario 'The user can add a bookmark to Bookmark manager' do 
         visit('/bookmarks/new')
         fill_in('url', with: 'http//www.testbookmark.com')
+        fill_in('title', with: 'Test Bookmark')
         click_button 'Submit'
-        expect(page).to have_content 'http//www.testbookmark.com'
+        
     end 
 end 
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -1,23 +1,16 @@
 require 'pg'
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    
     visit ('/bookmarks')
     
-    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
   
   end
 
   scenario 'Visiting /bookmarks shows me all the bookmarks' do 
-    
-
-  
-  Bookmark.create(url: "http://www.makersacademy.com")
-  Bookmark.create(url: "http://www.destroyallsoftware.com")
-  Bookmark.create(url: "http://www.google.com")
-
+  Bookmark.create(url: "http://www.makersacademy.com", title: 'Makers Academy')
   visit('/bookmarks')
   end
 end
